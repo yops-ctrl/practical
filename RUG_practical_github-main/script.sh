937ab48352142cb19ebfd7c0d0635f75295c18a4
yopc <i.spyropoulos.1@student.rug.nl>
2024-01-29 15:36:10 +0200

    fixed it

--- RUG_practical_github-main/script.sh
+++ RUG_practical_github-main/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=$(ls ~/Work/PCfB/Practicals/Monday29/data)
+files=$(ls ./data)
 
 #get the length for each sequence in fasta file(s), keep the name
 for file in ${files}
@@ -10,7 +10,7 @@ awk '/^>/ { # header pattern detected
         if (seqlen){
          # print previous seqlen if exists 
          print seqlen
-         }
+}
 
          # pring the tag 
          print
@@ -35,10 +35,10 @@ done
 number=$(cut -d: -f 2 lengths.txt | sort -n | tail -n 1)
 
 #get the fasta header corresponding to the number, keep unique, and get rid of ">"; -w option for grep is the length of the longest
-test=$(grep -B 1 -w 481 lengths.txt | grep '>' | sort -u | cut -c2-)
+test=$(grep -B 1 -w 841 lengths.txt | grep '>' | sort -u | cut -c2-)
 
 #Print the output!
-echo "The longest sequence(s) "${test}" is or are "$number" amino acids long."
+echo "The longest sequence(s) "${test}" is or are "${number}" amino acids long."
 
 #delete temporary file
 rm ./lengths.txt
